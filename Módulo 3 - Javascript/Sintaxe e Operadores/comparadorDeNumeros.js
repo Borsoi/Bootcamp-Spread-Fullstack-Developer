8fbb2a2f15ed473d9298bb1f7a4b9baaa35b7caf
guilherme.borsoi <borsoi.guilherme@gmail.com>
2022-05-20 13:41:01 -0300

    Trocando let por const e formatando código

--- Módulo 3 - Javascript/Sintaxe e Operadores/comparadorDeNumeros.js
+++ Módulo 3 - Javascript/Sintaxe e Operadores/comparadorDeNumeros.js
@@ -1,20 +1,29 @@
 function compararNumeros(num1, num2) {
-    let boolean1 = num1 === num2 ? " são" : " não são"
-    let soma = num1 + num2
-    let boolean2 = soma >= 10 ? " maior" : " menor"
-    let boolean3 = soma >= 20 ? " maior" : " menor"
+  const boolean1 = num1 === num2 ? " são" : " não são";
+  const soma = num1 + num2;
+  const boolean2 = soma >= 10 ? " maior" : " menor";
+  const boolean3 = soma >= 20 ? " maior" : " menor";
 
-    const resultado = "Os números " + num1 + " e " + num2 +  boolean1 +  " iguais. Sua soma é " + soma + ", que é" + boolean2 + " que 10 e" +  boolean3 + " que 20."
+  const resultado =
+    "Os números " +
+    num1 +
+    " e " +
+    num2 +
+    boolean1 +
+    " iguais. Sua soma é " +
+    soma +
+    ", que é" +
+    boolean2 +
+    " que 10 e" +
+    boolean3 +
+    " que 20.";
 
-    return resultado
+  return resultado;
 }
 
-console.log(compararNumeros(1,2));
-console.log(compararNumeros(1,1));
-console.log(compararNumeros(5,6));
-console.log(compararNumeros(6,6));
-console.log(compararNumeros(10,12));
-console.log(compararNumeros(11,11));
-
-
-
+console.log(compararNumeros(1, 2));
+console.log(compararNumeros(1, 1));
+console.log(compararNumeros(5, 6));
+console.log(compararNumeros(6, 6));
+console.log(compararNumeros(10, 12));
+console.log(compararNumeros(11, 11));
