94d83a939876a2cce4729c00285407f5a121a51f
Borsoi <borsoi.guilherme@gmail.com>
2022-05-21 00:05:29 -0300

    Deletado flag não utilizada

--- Módulo 3 - Javascript/Variáveis e Tipos/substituidorDePares.js
+++ Módulo 3 - Javascript/Variáveis e Tipos/substituidorDePares.js
@@ -2,7 +2,6 @@ function separarParesPorZero(array) {
     if (!array) {
         return -1;
     } else {
-        flag = 0;
         while (array.some(item => item % 2 === 0 && item !== 0)) {
             let index = array.findIndex(item => item % 2 === 0 && item !== 0)
             array[index] = 0;
