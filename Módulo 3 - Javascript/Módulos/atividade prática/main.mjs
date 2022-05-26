14a618ae12a5a42a3ce4ea9da84e7b4e6240ce50
Borsoi <borsoi.guilherme@gmail.com>
2022-05-25 22:41:43 -0300

    Adicionado atividade prática

---
+++ Módulo 3 - Javascript/Módulos/atividade prática/main.mjs
@@ -0,0 +1,7 @@
+import { mostraIdade, mostraCidade, mostraHobby} from "./funcoes.mjs";
+
+console.log(mostraIdade("Guilherme", 24));
+console.log(mostraCidade("Guilherme", "São Leopoldo"));
+console.log(mostraHobby("Guilherme", "jogar videogame"));
+
+
