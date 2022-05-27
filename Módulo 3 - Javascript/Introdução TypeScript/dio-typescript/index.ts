8967adde545b1462605450731adf614ac7a6b771
guilherme.borsoi <borsoi.guilherme@gmail.com>
2022-05-27 17:43:00 -0300

    Adicionado aulas de interfaces e types

--- Módulo 3 - Javascript/Introdução TypeScript/dio-typescript/index.ts
+++ Módulo 3 - Javascript/Introdução TypeScript/dio-typescript/index.ts
@@ -1,1 +1,35 @@
-console.log("TypeScript");
+function soma(a: number, b: number) {
+  return a + b;
+}
+
+//INTERFACE
+interface IAnimal {
+  nome: string;
+  tipo: "terreste" | "aquático";
+  domestico: boolean;
+}
+
+interface IFelino extends IAnimal {
+  visaoNoturna: boolean;
+}
+
+const felino: IFelino = {
+  nome: "Leão",
+  tipo: "terreste",
+  visaoNoturna: true,
+  domestico: false,
+};
+
+//TYPES
+interface ICanino extends IAnimal {
+  porte: "Pequeno" | "Médio" | "Grande";
+}
+
+type IDomestico = IFelino | ICanino;
+
+const animal: IDomestico = {
+  nome: "Cachorro",
+  tipo: "terreste",
+  porte: "Médio",
+  domestico: true,
+};
